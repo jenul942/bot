a425ae9264cf8ce23382f3ca6cfc194f6a38ea69
Jenul Ranthisa <76620261+jenul942@users.noreply.github.com>
2021-09-03 19:35:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import os
 import telebot
-
+import requests
+from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot("1987421018:AAHfq6aNGFaWSGwDpNNBl1rNsXhRdLJHxRQ")
 
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm jenul Chat Bot")
+  bot.reply_to(message, "Hello! I will give you covid 19 states in sri lanka")
 
 
-@bot.message_handler(commands=["how"])
-def send_message(message):
-  bot.send_message(message.chat.id, "jenul")
+@bot.message_handler(commands=['deaths'])
+def scrap(message):
+  page = requests.get('https://www.worldometers.info/coronavirus/country/sri-lanka/')
+  after_bs = BeautifulSoup(page.content, 'html.parser')
+  find_data = after_bs.find_all(id="maincounter-number")
+  output = ''
+  for x in find_data:
+    output = output + x.text
+  bot.reply_to(message, output)
+
+@bot.message_handler(commands=['deaths'])
+def scrap(message):
+  page = requests.get('https://www.worldometers.info/coronavirus/country/sri-lanka/')
+  after_bs = BeautifulSoup(page.content, 'html.parser')
+  find_data = after_bs.find_all(id="maincounter-wrap")
+  output = ''
+  for x in find_data:
+    output = output + x.text
+  bot.reply_to(message, output)
+
 
 
 
